bd82d8b338bfb98420d7d8019092142db0c8d71a
modeste2022 <landry.monthe@facsciences-uy1.cm>
2025-10-08 15:09:22 +0200

    Clignotement de la LED

--- exercice1.py
+++ exercice1.py
@@ -1,4 +1,5 @@
 import machine
+import utime
 
 # Définition des broches
 LED = machine.Pin(16, machine.Pin.OUT)
@@ -11,8 +12,10 @@ press_count = 0
 def wait_for_press():
     while BUTTON.value() == 0:
         pass
+    utime.sleep(0.2)  # anti-rebond
     while BUTTON.value() == 1:
         pass
+    utime.sleep(0.2)
 
 while True:
     wait_for_press()
@@ -26,9 +29,10 @@ while True:
 
         if press_count == 1:
             LED.toggle()
-
+            utime.sleep(1)  # 0.5 Hz (1s ON, 1s OFF)
         elif press_count == 2:
             LED.toggle()
-
+            utime.sleep(0.3)  # plus rapide
         elif press_count == 3:
             LED.value(0)  # éteindre la LED
+            utime.sleep(0.1)
